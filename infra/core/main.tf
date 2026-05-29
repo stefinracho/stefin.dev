1d2e0387b81e64ed0bf84da88d10801ccdaac92e
Stefin Racho <106283464+stefinracho@users.noreply.github.com>
2026-05-29 12:40:46 -0700

    chore: trigger terraform to reconcile state (#10)

--- infra/core/main.tf
+++ infra/core/main.tf
@@ -60,7 +60,8 @@ module "security_group" {
 
   name        = "${var.project_prefix}-web-sg"
   description = "Security group for web server allowing HTTP/HTTPS globally"
-  vpc_id      = module.vpc.vpc_id
+
+  vpc_id = module.vpc.vpc_id
 
   ingress_with_cidr_blocks = [
     {
